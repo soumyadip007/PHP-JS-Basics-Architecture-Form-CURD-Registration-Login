0657a214f04feb02f313df4b33db16768427b2f2
soumyadip007 <soumyadip.note@gmail.com>
2019-06-27 21:19:05 +0530

    Session added

--- PHP/13-session.php
+++ PHP/13-session.php
@@ -10,7 +10,10 @@ session_start();
 // Set session variables
 $_SESSION["favcolor"] = "green";
 $_SESSION["favanimal"] = "cat";
-echo "Session variables are set.";
+echo "Session variables are set.<br>";
+echo $_SESSION['favcolor']."<br>";
+
+echo $_SESSION['favanimal']."<br>";
 ?>
 
 </body>
