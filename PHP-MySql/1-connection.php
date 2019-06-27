e21014d119568f18950c6e7d0dbea3a9244fd913
soumyadip007 <soumyadip.note@gmail.com>
2019-06-27 21:57:16 +0530

    DataBase Connection

---
+++ PHP-MySql/1-connection.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=exam", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
