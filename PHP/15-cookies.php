437be7dbadbf4f41d962fbd1cb248f49a75fc99d
soumyadip007 <soumyadip.note@gmail.com>
2019-06-27 21:47:35 +0530

    Cookies config

---
+++ PHP/15-cookies.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<?php
+$cookie_name = "user";
+$cookie_value = "Alex Porter";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+?>
+<html>
+<body>
+
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+     echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+     echo "Cookie '" . $cookie_name . "' is set!<br>";
+     echo "Value is: " . $_COOKIE[$cookie_name];
+}
+?>
+
+<p><strong>Note:</strong> You might have to reload the page to see the new value of the cookie.</p>
+
+</body>
+</html>
