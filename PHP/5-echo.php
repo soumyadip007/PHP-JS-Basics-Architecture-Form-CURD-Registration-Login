56ca6bf747e3ff8b0939c030b1cb62b0e6448ece
soumyadip007 <soumyadip.note@gmail.com>
2019-06-27 20:20:21 +0530

    Echo

---
+++ PHP/5-echo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "Team-Cognito";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!";
+?>
+
+
+</body>
+</html>
