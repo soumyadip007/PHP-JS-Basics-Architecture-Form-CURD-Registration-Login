c71f7b63aebf6681feb7cc4f6f2fb734da2b34ef
soumyadip007 <soumyadip.note@gmail.com>
2019-06-27 22:11:15 +0530

    Insert added

---
+++ PHP-MySql/2-Insert.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "exam";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO mat (user, friend)
+VALUES ('John', 'Doe')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
