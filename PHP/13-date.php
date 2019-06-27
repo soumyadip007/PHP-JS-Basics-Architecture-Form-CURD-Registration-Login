a77275a32a777c0b193c8956437a1a2817beea8c
soumyadip007 <soumyadip.note@gmail.com>
2019-06-27 21:02:06 +0530

    Date commit

---
+++ PHP/13-date.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+
+</body>
+</html>
