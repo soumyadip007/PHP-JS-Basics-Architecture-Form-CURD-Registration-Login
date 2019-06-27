51b2bd89b1bf73d66f7577023b4339bfbc81bc76
soumyadip007 <soumyadip.note@gmail.com>
2019-06-27 22:17:41 +0530

    DB Changed & Database Config Successfully

--- PHP-MySql/1-connection.php
+++ PHP-MySql/1-connection.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=exam", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=resell", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully"; 
--- PHP-MySql/2-Insert.php
+++ PHP-MySql/2-Insert.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "exam";
+$dbname = "resell";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -11,14 +11,15 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "INSERT INTO mat (user, friend)
-VALUES ('John', 'Doe')";
+$sql = "INSERT INTO account (email, name,mobile, pass)
+VALUES ('John@gmail.com', 'John', '7687028083', 'password')";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
+
 
 mysqli_close($conn);
 ?>
