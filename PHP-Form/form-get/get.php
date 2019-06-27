f003924bc79ed63422e57033f983b7b377476a7a
soumyadip007 <soumyadip.note@gmail.com>
2019-06-27 20:48:47 +0530

    GET Config

---
+++ PHP-Form/form-get/get.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+</body>
+</html>
--- PHP-Form/form-get/post.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-
-</body>
-</html>
