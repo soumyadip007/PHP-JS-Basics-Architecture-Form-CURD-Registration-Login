924adc872e17cf3a172be3bbea01fb7887c25070
soumyadip007 <soumyadip.note@gmail.com>
2019-06-27 20:15:51 +0530

    Hello-Case

---
+++ PHP/3-case.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo $x;
+?>
+ 
+<?php
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+?>
+ 
+</body>
+</html>
