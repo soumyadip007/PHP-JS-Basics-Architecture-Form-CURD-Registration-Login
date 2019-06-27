7349ae7b43e3249e3f1924052545a3a49b26d966
soumyadip007 <soumyadip.note@gmail.com>
2019-06-27 20:33:17 +0530

    Function config

--- PHP/10-function.php
+++ PHP/10-function.php
@@ -8,6 +8,16 @@ function writeMsg() {
 }
 
 writeMsg();
+
+function familyName($fname) {
+    echo "$fname Refsnes.<br>";
+}
+
+familyName("Jani");
+familyName("Hege");
+familyName("Stale");
+familyName("Kai Jim");
+familyName("Borge");
 ?>
 
 </body>
