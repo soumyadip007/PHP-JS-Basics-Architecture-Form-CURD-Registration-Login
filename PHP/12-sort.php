462d8335417eaf9b02794cbaf97cd21fdbb3fa67
soumyadip007 <soumyadip.note@gmail.com>
2019-06-27 20:37:24 +0530

    Sort Config

---
+++ PHP/12-sort.php
@@ -0,0 +1,18 @@
+Result Size: 753 x 573
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$numbers = array(4, 6, 2, 22, 11);
+sort($numbers);
+
+$arrlength = count($numbers);
+for($x = 0; $x < $arrlength; $x++) {
+    echo $numbers[$x];
+    echo "<br>";
+}
+?>
+
+</body>
+</html>
